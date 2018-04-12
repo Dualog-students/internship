7cfeb7cccfe454b002d0c692e77ee1f1c35006c2
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2018-04-12 15:36:29 +0200

    Update fonts to poppins

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -15,15 +15,21 @@ const LandingPage = (props, context) => {
       <main style={{ color: colorPrimary }}>
         <BrowserNotes />
         <div className="intro-wrapper">
-          <div className="intro-name">We are looking for summer interns!</div>
+          <div className="intro-name">Dualog is looking for summer interns!</div>
           <div className="tagline">
           <p><b>Who are you?</b></p>
-          You are an IT student at the university
+          An IT student with a passion for developing software.
           <br />
           <br />
           <p><b> Who are we?</b></p>
-          A global company with   
-            
+          A global company that delivers software to the maritime industry.
+          <br />
+          <br />
+          <p><b> What to do?</b></p>
+          Arrange a hackathon, then develop an idea based on it.
+          <br />
+          <br />
+            <b><a href="mailto:fredrik@dualog.com?subject=Dualog%20Summer%20internship%202018%20-%20YOUR_NAME"> APPLY NOW! </a></b>
           </div>
           <SocialIcons />
         </div>
